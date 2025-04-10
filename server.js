aa88bce283d7044fb2bd2060130c6d9b5ee37700
Tomi126 <115153488+Tomi126@users.noreply.github.com>
2025-04-10 14:16:03 +0200

    +2 lekérdezés

--- server.js
+++ server.js
@@ -25,6 +25,24 @@ app.get("/szobak",(req,res)=>{
     })
 })
 
+//azok a szobák kilistázása, ahol 3 vagy több ágy van, ágyak szerint növekvő sorrendben
+app.get("/szobak3",(req,res)=>{
+    const sql = "SELECT * FROM szobak WHERE agy>=3 ORDER BY agy ASC"
+    db.query(sql,(err,result)=>{
+        if(err) return res.json(err)
+        return res.json(result)
+    })
+})
+
+//azok a foglalások kilistázása, ahol az érkezés dátuma 2016.01.10-tól  2016.01.20 -ig van.
+app.get("/foglalas",(req,res)=>{
+    const sql = "SELECT * FROM foglalasok WHERE erk LIKE '2016-01-1%'"
+    db.query(sql,(err,result)=>{
+        if(err) return res.json(err)
+        return res.json(result)
+    })
+})
+
 
 app.listen(3000, () => {
     console.log("A szerver a 3000-es porton fut.")
